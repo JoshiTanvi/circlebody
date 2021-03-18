f29f51437c66f17afb01e0fa99f85855a0ff3208
JoshiTanvi <tanvijoshi04@gmail.com>
2021-03-18 19:34:23 +0530

    Update circel.js

--- circel.js
+++ circel.js
@@ -4,10 +4,9 @@ class circel {
        var options = {
            'restitution':0.2,
            'friction':1.0,
-           'density':1.0,
-          // 'isStatic': true
+           'density':1.0
        }
-     this.body = Bodies.circle(x, y, 50, options)
+     this.body = Bodies.circle(x, y, 50)
      //this.daimeter = d
    
      World.add(world, this.body);
@@ -20,9 +19,9 @@ class circel {
        push();
       // rectMode(CENTER)
          fill("blue");
-        // translate(this.body.position.x, this.body.position.y);
-        // rotate(this.body.angle);
-         circle(this.body.position.x,this.body.position.y, 100);
+         translate(this.body.position.x, this.body.position.y);
+         rotate(this.body.angle);
+         circle(0,0, 100);
          pop();
        }
    
